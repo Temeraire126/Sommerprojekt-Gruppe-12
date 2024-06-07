b9338c1eb718aefe8f37287a87e80f5efd133bf3
LanaAbd26 <abdullatif.lana@gmail.com>
2024-06-07 18:07:20 +0200

    Update RFID_TestVersuch.ino
    RFID Code is working now!

--- Beispielprogramme/RFID_TestVersuch.ino
+++ Beispielprogramme/RFID_TestVersuch.ino
@@ -3,34 +3,35 @@
 #include <Servo.h>
 
 //Anschlüsse definieren 
-#define SDS_Pin 10
-#define RST_Pin 9
+#define SDA_Pin D1
+#define RST_Pin D4
 
 //LEDs
-# define LED_RED
-# define LED_GREEN
+# define LED_RED D2
+# define LED_GREEN D3
 
 // Zustand der LEDs false = beim Start ausgeschaltet
 bool StatusroteLED = false;
 bool StatusgrueneLED = false;
 
 //RFID-Empfänger bennen, Pins zuordnen
-MFRC522 rfid(SDS_Pin, RST_Pin);
+MFRC522 mfrc522(SDA_Pin, RST_Pin);
 
 
-Servo myservo;
-  int pos = 0;
+//Servo myservo;
+  //int pos = 0;
   
 void setup() {
-  myservo.attach();
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
+  //myservo.attach();
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   SPI.begin();
 
-  //Initialsierung des RFID-Empfängers
-  rfid.PCD_Init();
+   //Initialsierung des RFID-Empfängers
+  mfrc522.PCD_Init();
+  
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
   }
 
 void loop()
@@ -38,14 +39,14 @@ void loop()
 String WertDEZ;
 
 //Wenn keine Karte in Reichweite ist...
-if (!rfid.PICC_IsNewCardpresent())
+if (!mfrc522.PICC_IsNewCardPresent())
 {
 //... wird die Abfrage wiederholt.
 return;
 }
 
 //Wenn kein RFID-Sender ausgewählt wurde...
-if (!rfid.PICC_ReadCardSerial())
+if (!mfrc522.PICC_ReadCardSerial())
 {
 //...wird die Abfrage wiederholt.
 return;
@@ -54,10 +55,10 @@ return;
 Serial.println("Karte entdeckt!");
 
 //Dezimal-Wert in Strings schreiben
-for (byte i = 0; i < MFRC522.uid.size; i++) //über jedes Byte der UID der Karte
+for (byte i = 0; i < mfrc522.uid.size; i++) //über jedes Byte der UID der Karte
 {
 //String zusammenbauen
-WertDEZ = WertDEZ + String(MFRC522.uid.uidByte[i], DEC) + " "; 
+WertDEZ = WertDEZ + String(mfrc522.uid.uidByte[i], DEC) + " "; 
 }
 
 //trim() -> Leerzeichen am Ende des Strings entfernen
@@ -66,31 +67,31 @@ WertDEZ.trim();
 //Kennung dezimal anzeigen
 Serial.println("Dezimalwert: " + WertDEZ);
 
-//rote LED, wenn Chip falsch ist
-if (WerteDEZ != " 1 ")
+//gruene LED, wenn Chip richtig ist
+if (WertDEZ == "39 123 81 211")
 {
-
-//rote LED schalten
-StatusroteLED = !StatusroteLED;
-digitalWrite(LED_RED, StatusroteLED);
-  delay(2000);
+//gruene LED schalten
+StatusgrueneLED = !StatusgrueneLED;
+digitalWrite(LED_GREEN, StatusgrueneLED);
+digitalWrite(LED_RED, LOW);
 }
 
-//gruene LED schalten
-if (WerteDEZ == " 1 ")
+// Status eandern: rote LED schalten bei falschem Chip
+if (WertDEZ == "169 140 161 226")
 {
-LED_GREEN = !LED_GREEN;
-digitalWrite(GREEN_LED,StatusgrueneLED);
-openSesame();
-  delay(2000);
+  Serial.println("chip 2");
+StatusroteLED = !StatusroteLED;
+digitalWrite(LED_RED,StatusroteLED);
+digitalWrite(LED_GREEN, LOW);
+//openSesame();
 }
 //Wartezeit verhindert, dass die Karte nur einmal erkannt wird
-delay(5000);
-closeSesame();
+delay(2000);
+//closeSesame();
 }
 
 //Methoden für den Servo
-void openSesame(){
+/*void openSesame(){
 for (pos = 0; pos <= 180; pos += 1){ //goes from 0 degrees to 180 degrees in steps of 1 degree
 myservo.write(pos);                  //tell servo to go to position in variable 'pos'
 delay(15);                           //waits 15ms for the servo to reach the position
@@ -102,4 +103,4 @@ for (pos = 0; pos <= 180; pos += 1){ //goes from 0 degrees to 180 degrees in ste
 myservo.write(pos);                  //tell servo to go to position in variable 'pos'
 delay(15);                           //waits 15ms for the servo to reach the position
 }
-}
+}*/
