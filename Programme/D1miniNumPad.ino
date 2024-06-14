7c426f06d650d02a0c1c3aa564bba0b078855d6c
LanaAbd26 <abdullatif.lana@gmail.com>
2024-06-14 15:59:37 +0200

    Update D1miniNumPad.ino

--- Programme/D1miniNumPad.ino
+++ Programme/D1miniNumPad.ino
@@ -13,10 +13,10 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len){
 const byte ROWS = 4;
 const byte COLS = 4;
 char keys[ROWS][COLS] = {
-  { '1', '2', '3', 'A' },
-  { '4', '5', '6', 'B' },
-  { '7', '8', '9', 'C' },
-  { '*', '0', '#', 'D' }
+  {'D','#','0','*'},
+  {'C','9','8','7'},
+  {'B','6','5','4'},
+  {'A','3','2','1'}
 };
 byte rowPins[ROWS] = { 5, 4, 3, 2 };  //connect to the row pinouts of the keypad
 byte colPins[COLS] = { 9, 8, 7, 6 };  //connect to the column pinouts of the keypad
