4f8371d59c12fc408067819b5b394682a08c55fe
LanaAbd26 <abdullatif.lana@gmail.com>
2024-06-14 16:41:42 +0200

    Update D1miniTest.ino

--- Beispielprogramme/TestHardware/D1miniTest.ino
+++ Beispielprogramme/TestHardware/D1miniTest.ino
@@ -3,36 +3,39 @@
 const byte ROWS = 4;
 const byte COLS = 4;
 char keys[ROWS][COLS] = {
-  { '1', '2', '3', 'A' },
-  { '4', '5', '6', 'B' },
-  { '7', '8', '9', 'C' },
-  { '*', '0', '#', 'D' }
+   {'D','#','0','*'},
+   {'C','9','8','7'},
+   {'B','6','5','4'},
+   {'A','3','2','1'}
 };
 //D0,D1,D2,D4
-byte rowPins[ROWS] = { 16, 5, 4, 2 };  //connect to the row pinouts of the keypad
+byte rowPins[ROWS] = { D4, D3, D2, D1 };  //connect to the row pinouts of the keypad
 //D5,D6,D7,D8
-byte colPins[COLS] = { 14, 12, 13, 15 };  //connect to the column pinouts of the keypad
+byte colPins[COLS] = { D8, D7, D6, D5 };  //connect to the column pinouts of the keypad
+
+#define PIN_OUT D0
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-#define PIN_OUTPUT D3
-
+char Taste;
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(PIN_OUT, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   char key = keypad.getKey();
 
+
   if (key) {
     Serial.println(key);
     char code[] = {'0','0','0','0'};
     if(checkCode(code, key)){
-      digitalWrite(LED_BUILTIN,HIGH);
-      delay(500);
-      digitalWrite(LED_BUILTIN,LOW);
+      digitalWrite(PIN_OUT,HIGH);
+      delay(5000);
+      digitalWrite(PIN_OUT,LOW);
     }
   }
 }
@@ -51,10 +54,16 @@ boolean checkCode(char code[], char first) {
       input[index] = key;
       index += 1;
       if (index == sizeof(code)) {
-        return strcmp(input, code);
+        Serial.println(input);
+        Serial.println(code);
+        bool cmp = true;
+        for (int i = 0; i<sizeof(code);i++){
+          if(input[i]!=code[i]){cmp=false;}
+        }
+        return cmp;
       }
     }
-    delay(1000);
+    delay(10);
     time += 1;
   }
   return false;
