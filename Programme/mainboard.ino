864e0a6affdb01e1d748bdf831b036a2ad2f7aa8
nis8584 <stanislawmatis@gmail.com>
2024-06-21 11:08:33 +0200

    Update mainboard.ino

--- Programme/mainboard.ino
+++ Programme/mainboard.ino
@@ -17,32 +17,43 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);  //Hier wird festgelegt um was für einen Di
 //- SDA: D2   weiß
 
 const int codeLength =4;
-//adressen festlegen 
-//Adressen der D1Mini Keypads (passcode wird hierhin gesendet)
- uint8_t addressMini1[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};//0xC8,0xC9,0xA3,0x14,0x32,0x59
- uint8_t addressMini2[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};//0x60,0x01,0x94,0x10,0x09,0x3C
-//Adressen von denen der Status der Tresore erhalten wird (wird zur Unterscheidung der Tresore verwendet)
- uint8_t recvAddress1[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};//0x4C,0x75,0x25,0x36,0xBD,0x64
- uint8_t recvAddress2[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};//0x08,0x3A,0x8D,0xCF,0xAF,0x55
+//für weitere Tresore die Adressse im gleichen Vormat speichern:
+  //Adressen der D1Mini Keypads (passcode wird hierhin gesendet)
+  uint8_t addressMini1[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};//0xC8,0xC9,0xA3,0x14,0x32,0x59
+  uint8_t addressMini2[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};//0x60,0x01,0x94,0x10,0x09,0x3C
+ //uint8_t addressMini3[] = ....
+
+  //Adressen der NodeMCUs von denen der Status der Tresore erhalten wird (wird zur Unterscheidung der Tresore verwendet)
+  uint8_t recvAddress1[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};//0x4C,0x75,0x25,0x36,0xBD,0x64
+  uint8_t recvAddress2[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};//0x08,0x3A,0x8D,0xCF,0xAF,0x55
+  //uint8_t recvAddress3[]=...
+
 //pakate definieren und initialisieren
+//sendet:
  typedef struct packageCodeStruct{
   String a;
  } packageCodeStruct;
+//erhält:
  typedef struct packageResponseStruct{
   bool b;
  } packageResponseStruct;
 
  packageCodeStruct sendData;
+//eine variable für jeden Tresor, der einen Wert zurückschickt. So wird sich der Status aller Schlüssel gemerkt.
  packageResponseStruct recvData1;
  packageResponseStruct recvData2;
+//packageResponseStrct recvData3;
+//...
 
 //Sind Schlüssel in den Tresoren?
 bool statusTresor1=true;
 bool statusTresor2=true;
+//bool statusTresor3...
 
-bool codeIsDisplayed = false; // do we need this?
+bool codeIsDisplayed = false; 
 
 //Aufruf bei Datenerhalt. Abhängig der Senderadresse wird der Inhalt in verschiedene variablen gespeichert
+//Bei weiteren Tresoren muss für jeden neuen eine eigene Abfrage gemacht werden
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len){
   if(0==memcmp(recvAddress1, mac , sizeof(mac))){
   memcpy(&recvData1, incomingData,sizeof(recvData1));
@@ -50,10 +61,15 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len){
   if(0==memcmp(recvAddress2, mac, sizeof(mac))){
   memcpy(&recvData2, incomingData,sizeof(recvData2));
   }
+ /*
+    if(0==memcmp(recvAddress3, mac, sizeof(mac))){
+  memcpy(&recvData3, incomingData,sizeof(recvData3));
+  }
+  */
   Serial.print("received");
   updateLED();
 }
-
+// Debugginghilfe beim Versenden der Passcodes:
 void onDataSent(uint8_t *mac_addr, uint8_t sendstatus){
   Serial.print("Last Packet Send Status: ");
   if(sendstatus == 0){
@@ -97,6 +113,8 @@ String generateCode(){
   return rtr;
 }
 //stellt den Status der Tresore in den LEDs dar (grün=schlüssel ist vorhanden und kann abgeholt werden. rot= schlüssel wurde entfernt und wird verwendet)
+//auch hier wäre eine Erweiterung bei neuen Tresoren notwending. Bei mehr als 2 oder 3 Tresoren wäre es sinnvoller das ganze über einen for-Schleife laufen zu lassen und die outputs vorher in einem Array zu speichern
+
 void updateLED(){
   statusTresor1=recvData1.b;
   statusTresor2=recvData2.b;
@@ -143,4 +161,10 @@ void loop() {
     esp_now_send(addressMini2, (uint8_t *)&sendData,sizeof(sendData));
     delay(1000);   
   }
+ /*
+ delay(100);
+ if(digitalRead(BUTTON3)==LOW){
+ ...
+ }
+ */
 }
